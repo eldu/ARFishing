0e01b6c74dc2aae6c6f1b426dfd378b2ecb345e4
nnewberg <nnewberg@seas.upenn.edu>
2016-12-07 19:59:02 -0500

    basic billboard working

--- Assets/Scripts/VoiceCommands.cs
+++ Assets/Scripts/VoiceCommands.cs
@@ -8,7 +8,7 @@ public class VoiceCommands : MonoBehaviour {
     public GameObject MenuPrefab;
     private GameObject menuGameObject;
 
-    public AudioSource audio;
+    public AudioSource BGMaudio;
 
     // Use this for initialization
     void Start () {
@@ -22,19 +22,21 @@ public class VoiceCommands : MonoBehaviour {
 
     public void ShowMenu()
     {
-        menuGameObject = (GameObject)Instantiate(MenuPrefab);
-        System.Diagnostics.Debug.WriteLine("Showing menu");
+        Vector3 offset = new Vector3();
+        offset.z = 0.5f;
+        menuGameObject = (GameObject)Instantiate(MenuPrefab, offset + Camera.main.transform.position, MenuPrefab.transform.rotation);
+        UnityEngine.Debug.Log("Menu???");
     }
 
     public void StopMusic()
     {
-        if (!audio.mute)
-            audio.mute = true;
+        if (!BGMaudio.mute)
+            BGMaudio.mute = true;
     }
 
     public void StartMusic()
     {
-        if (audio.mute)
-            audio.mute = false;
+        if (BGMaudio.mute)
+            BGMaudio.mute = false;
     }
 }
