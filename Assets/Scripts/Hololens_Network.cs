947d133075bd2c33b82dfb796a59c0a4f5fb9345
Kangning Li <likangning93@gmail.com>
2016-12-07 22:39:05 -0500

    able to create room on Gary's photon account

--- Assets/Scripts/Hololens_Network.cs
+++ Assets/Scripts/Hololens_Network.cs
@@ -3,43 +3,49 @@ using System.Collections;
 
 public class Hololens_Network : Photon.PunBehaviour
 {
-    // This is for Paint Ball networking at PC
-    //     if you are looking for LOOK-1.b, please refer to PCNetwork_Cube.cs 
     string roomName;
 
+    // LOOK-1.b: creating a room on PC
     void Start()
     {
+        // Make sure "Auto-Join Lobby" was checked at 
+        //   Assets-> Photon Unity Networking-> Resources-> PhotonServerSettings
+        //   so the application will automatically connect to Lobby
+        //   and call OnJoinedLobby()
         PhotonNetwork.ConnectUsingSettings("0.1");
         roomName = GenerateRoomName();
     }
 
-    public override void OnJoinedLobby()
-    {
-        PhotonNetwork.CreateRoom(roomName);
-    }
-
-    public override void OnPhotonJoinRoomFailed(object[] codeAndMsg)
-    {
-        base.OnPhotonJoinRoomFailed(codeAndMsg);
-    }
-
-    public override void OnCreatedRoom()
-    {
-        base.OnCreatedRoom();
-    }
-
     static string GenerateRoomName()
     {
-        const string characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        const string characters = "abcdefghijklmnopqrstuvwxyz0123456789"; //add the characters you want
 
         string result = "";
 
-        int charAmount = Random.Range(4, 6);
+        int charAmount = Random.Range(4, 6); //set those to the minimum and maximum length of your string
         for (int i = 0; i < charAmount; i++)
         {
             result += characters[Random.Range(0, characters.Length)];
         }
 
         return result;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //PhotonNetwork.CreateRoom(null);
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    // Look-1.b: We are not doing anything in the functions below
+    // , but you may want to do something at the corresponding mobile function
+    // On mobile client, use OnJoinedRoom() instead of OnCreatedRoom()
+    public override void OnPhotonJoinRoomFailed(object[] codeAndMsg)
+    {
+        base.OnPhotonJoinRoomFailed(codeAndMsg);
+    }
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
     }
 }
