fc390faf62b577118c94c2813cff436e589ba252
eldu <shi.ellenduong@gmail.com>
2016-12-08 00:36:05 -0500

    Updated wander, added lure

---
+++ Assets/Scripts/BaitComponent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaitComponent : MonoBehaviour {
+    private bool active;
+    public float strength = 5.0f; // Strength of attraction
+    public float distOfDetection = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        active = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool isActive()
+    {
+        return active;
+    }
+
+    public void setActive(bool val)
+    {
+        active = val;
+    }
+}
--- Assets/Scripts/Wander.cs
+++ Assets/Scripts/Wander.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class Wander : MonoBehaviour {
+    // Components
+    public GameObject bait;
+    private BaitComponent bc;
+
     public float speed = 0.5f;
+    public Vector3 range = new Vector3(10.0f, 2.0f, 10.0f); // Range of Motion in x y z directions
 
-    public float range = 10.0f;
-    public Vector3 wayPoint = new Vector3();
-
+    private Vector3 wayPoint = new Vector3();
     private float time = 0.0f;
 
     Rigidbody rb;
@@ -14,34 +17,54 @@ public class Wander : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
        rb = GetComponent<Rigidbody>();
-        generateWayPoint();
-        Vector3 dir = transform.forward;
-        rb.velocity = dir * speed;
+
+
+       GameObject[] bs = GameObject.FindGameObjectsWithTag("Lure");
+       bait = bs[0];
+
+       bc = bait.GetComponent<BaitComponent>();
+       time = -1.0f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        time += Time.deltaTime;
         // Go towards the wayPoint
 
-        if (time >= 5.0f) 
-        {
-            generateWayPoint();
+        Vector3 wayPoint = getWayPoint();
+        transform.LookAt(wayPoint);
 
-            Vector3 dir = transform.forward;
-            rb.velocity = dir * speed;
-            time = 0.0f;
+        Vector3 dir = wayPoint - transform.position; // Changes the direction that the fish looks at
+        rb.velocity = transform.forward * speed;
+
+        if (bc.isActive() && dir.magnitude < bc.distOfDetection);
+        {
+            Vector3 force = (bc.strength * 1.0f / dir.magnitude) * dir.normalized;
+            rb.AddForce(force);
         }
 
-	}
+        time += Time.deltaTime;
+    }
 
-    // Generate waypoint and look at point
-    private void generateWayPoint() {
-        wayPoint = new Vector3(Random.Range(transform.position.x - range, transform.position.x + range),
-        transform.position.y,
-        Random.Range(transform.position.z - range, transform.position.z + range));
+    private Vector3 getWayPoint()
+    {
+        if (time < 0.0f)
+        {
+        // Initial
+           time = 0.0f;
+           wayPoint = new Vector3(Random.Range(transform.position.x - range.x, transform.position.x + range.x),
+                                    Random.Range(transform.position.y - range.y, transform.position.y + range.y),
+                                    Random.Range(transform.position.z - range.z, transform.position.z + range.z));
+        } else if (bc.isActive()) {
+                wayPoint = bait.transform.position;
+        }
+        else if (time > 5.0f) {
+            time = 0.0f;
+            wayPoint = new Vector3(Random.Range(transform.position.x - range.x, transform.position.x + range.x),
+                                    Random.Range(transform.position.y - range.y, transform.position.y + range.y),
+                                    Random.Range(transform.position.z - range.z, transform.position.z + range.z));
+        }
 
-        transform.LookAt(wayPoint);
+        return wayPoint;
     }
 
     void OnSelect()
